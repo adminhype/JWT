66c3e29280652b4d2a05e769e5d8c8a124682082
adminhype <ado.designs@outlook.de>
2026-01-08 19:40:35 +0100

    feat: HttpOnly cookies with refreshview

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework_simplejwt.views import (
 
 from django.urls import path
 
-from .views import CustomTokenObtainPairView, RegistrationView, MoinWorldView
+from .views import CookieTokenRefreshView, CustomTokenObtainPairView, RegistrationView, MoinWorldView
 
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
 
     path('moin/', MoinWorldView.as_view(), name='moin'),
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -2,7 +2,9 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# from django.conf import settings
 
 from .serializers import RegistrationSerializer
 
@@ -56,4 +58,39 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         )
 
         response.data = {"message": "Tokens set in cookies"}
+        return response
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh_token')
+
+        if refresh_token is None:
+            return Response(
+                {"detail": "Refresh token not found in cookies."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = self.get_serializer(data={'refresh': refresh_token})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response(
+                {"detail": "Invalid refresh token."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        access_token = serializer.validated_data.get('access')
+
+        response = Response(
+            {"message": "Access token refreshed successfully."})
+        response.set_cookie(
+            key='access_token',
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+
         return response
